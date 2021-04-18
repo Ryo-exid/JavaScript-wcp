9d417a6272b1bd9d613cd4b63ed95ae156067e14
Ryo-exid <sakura4oneok@gmail.com>
2021-04-18 20:00:11 +0900

    [Finish]JavaScript4章(jQuery)

---
+++ JS4章/app.js
@@ -0,0 +1,19 @@
+// JavaScript4章
+
+$(document).ready(function () {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
+
+
+// 頻繁に使うコードなので、省略形も用意されています。
+
+// $(function(){
+//   // jQueryプログラムの内容
+// });
+
+
+// $( セレクタ ).メソッド ( パラメータ );
+
+
+// 今回のコードは、「bodyタグ内のHTMLを、
+// <h1>Hello JQuery!!</h1>で書き換えなさい」という意味になります。
