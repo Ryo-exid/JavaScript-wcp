85d6687e5350c755af74b19a09193a63f1bae401
Ryo-exid <sakura4oneok@gmail.com>
2021-04-22 20:45:26 +0900

    成功時と失敗時の処理を追記する

--- JS12章/script.js
+++ JS12章/script.js
@@ -41,3 +41,33 @@ $(function(){
 
 
 
+
+
+// 成功時と失敗時の処理を追記する
+// さらに、通信に成功した場合と失敗した場合の処理を追記します。
+
+
+// （API_KEY には、"取得したAPIキー" を記述）
+API_KEY = "取得したAPIキー";
+
+$(function(){
+  $('#btn').on('click', function() {
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      //通信成功
+    }).fail(function (data) {
+      //通信失敗
+    });  });
+});
+
+
+// .done()は通信に成功した場合、.fail()は失敗した場合に記述するメソッドです。
+// 「通信に成功する」とは、今回はWeb APIが正常に呼び出せたことを意味しています。
+// どちらのメソッドでも(data)を指定していますが、その引数内に取得した結果が入ります。（dataは任意の変数名）
+
+
+
+
